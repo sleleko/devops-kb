2eeb6b0c003b6c5feaf8d8d6d934648d442a133b
Sergey Leleko <sergey@leleko.ru>
2019-04-18 17:46:19 +0500

    added simple commands

--- docker/simpleDocker.sh
+++ docker/simpleDocker.sh
@@ -1,1 +1,14 @@
-.
+# simple run contaiter from Docker Image with tag latest in daemon mode, for example with debian docker image
+docker run -d debian:latest
+
+# simple run container in interactive mode
+docker run -it debian:latest
+
+# show current runned containers
+docker ps
+
+# show all containers on current node (runned and stopped)
+docker ps -a
+
+# show all docker images donloaded to this node
+docker images

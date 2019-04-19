9724953effa7f5c7fd08c43afecbe71903c2b065
Sergey Leleko <sergey@leleko.ru>
2019-04-19 11:02:23 +0500

    added sending volumes between two different nodes
    In cluster

--- zfs/zfs-work-commands.sh
+++ zfs/zfs-work-commands.sh
@@ -18,3 +18,7 @@ zfs snapshot rpool/srv/src@any-text-or-int-for-identification-snapshot -r
 
 # move zfs sub volumes from one parent volume to other, with all him snapsjots if its exist
 zfs rename rpool/srv/db/subvolume rpool/srv/old/db/subvolume
+
+# send volume from one node to two node via ssh. Note: this command runing on target node and with "pv" utility
+# for display progress bar of operation
+ssh sourcenode zfs send -v rpool/srv/db/somevolume | pv | zfs recv -Fv rpool/srv/db/somevolume 
